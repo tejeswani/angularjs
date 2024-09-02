7dcd17f6f7eadff4c60fdea9ce3f4637da64fd31
Tejeswani <tejeswanib@gmail.com>
2024-09-03 00:14:28 +0530

    Netflix login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { LoginComponent } from './tejeswani/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tejeswani/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  userName:any;
+  password:any;
+  emailId:any;
+  checkboxvalue:any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  submit(){
+    let obj={
+      "userName":this.userName,
+      "password":this.password,
+      "emailId":this.emailId,
+      "checkBox":this.checkboxvalue
+    }
+    console.log(obj)
+  }
+
+  checkBox(event:any){
+    this.checkboxvalue = event.target.checked;
+    console.log(this.checkboxvalue)
+  }
+  // imageUrl = 'src/assets/Images/netflix-login-page.jpg';
+
+}
